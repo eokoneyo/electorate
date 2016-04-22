47552dcfc9ac4422ac8a97ff81784e8b080df9f6
Eyo O. E <maziey93@gmail.com>
2016-04-22 13:32:55 +0100

    Add jagged edge to single page display!!

--- single.php
+++ single.php
@@ -5,9 +5,16 @@
 			<div class="article-jumbotron" style="background-image: url(<?php
 				$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 				echo $feat_image;?>);">
-				<h2 class="post-title center-align white-text"><?php the_title(); ?></h2>
+				<div class="tint"></div>
+				<div class="post-title">
+					<div class="container">
+						<h2 class="white-text"><?php the_title(); ?></h2>
+						<p class="white-text"><?php echo the_date('d F Y', false); ?></p>
+						<p >by <?php echo get_the_author(); ?></p>
+					</div>
+				</div>
 			</div>
-			<div class="container">
+			<div class="container post-content">
 				<div class="row">
 					<div class="col s12">
 						<?php the_content(); ?>
