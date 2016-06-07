cbcee90368c2a62036c69bc9199602544c016f97
Eyo O. E <maziey93@gmail.com>
2016-06-07 11:32:51 +0100

    rebase file for election theme

--- release_script.sh
+++ release_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gulp
-zip -r dante.zip ./* -x './node_modules/*' './lib/*' './bower.json' './gulpfile.js' './release_script'
+zip -r election_theme.zip ./* -x './node_modules/*' './lib/*' './bower.json' './gulpfile.js' './release_script'
