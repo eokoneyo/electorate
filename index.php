38fd884e1fdedd422c78d1e3be6e3bce04ae56df
Eyo O. E <maziey93@gmail.com>
2016-06-07 15:28:18 +0100

    style normal posts to be aside the first posts

--- index.php
+++ index.php
@@ -49,37 +49,17 @@
 									    </div>
 									  </div>
 							  	</div>
-						  	<?php else :?>
-						  		<div class="col s12 l4">
-							  		<div class="row">
-							  			<div class="col s12 l12">
-							  				<div class="card">
-											    <div class="card-image post-card">
-													<?php
-														// Output the featured image.
-														if ( has_post_thumbnail() ) :
-															?>
-															<div class="index-thumbnails" style="background-image: url(<?php
-																$feat_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); echo $feat_image;?>);">
-																<div class="tint"></div>
-																<div class="title">
-																	<h5 class="white-text"><a class="white-text" href="<?php the_permalink(); ?> ">  <?php the_title(); ?></a></h5>
-																	<p class="date"><?php echo the_date('d F Y', false); ?></p>
-																</div>
-															</div>
-															
-															<?php 
-															
-														else: ?>
-															<div class="noimage-content">
-																<h5 class=""><a class="grey-text text-darken-4" href="<?php the_permalink(); ?> ">  <?php the_title(); ?></a></h5>
-																<p class="date"><?php echo the_date('d F Y', false); ?></p>
-																<?php the_excerpt(); ?>
-															</div>
-													<?php
-														endif;
-													?>
-											    </div>
+							<?php endif;?>
+						<?php endwhile; ?>
+
+						<?php
+							$args = array('offset' => 1, );
+							$loop = new WP_Query( $args ); ?>
+							<div class="col s12 l4">
+							  	<div class="row">
+									<?php while ($loop->have_posts()): $loop->the_post() ?>
+								  		<div class="col s12 l12">
+								  			<div class="card">
 											    <div class="card-content row valign-wrapper">
 											    	 <a class="btn-floating btn-large waves-effect waves-light yellow lighten-1">
 											    	 <span class="fb-comments-count" data-href="<?php the_permalink(); ?> "></span><i class="fa fa-commenting-o" aria-hidden="true"></i></a>
@@ -92,11 +72,11 @@
 										            </div>
 											    </div>
 											</div>
-							  			</div>
-							  		</div>
+								  		</div>
+									<?php endwhile; ?>
 							  	</div>
-							<?php endif;?>
-						<?php endwhile; ?>
+							</div>
+
 					</div>
 				</div>
 			<?php else : ?>
