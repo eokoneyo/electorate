87cb640e94fc7eca457b9a267bfec4c21b34ae01
Eyo O. E <maziey93@gmail.com>
2016-06-08 08:52:39 +0100

    modify release script to exclude release script from build

--- release_script.sh
+++ release_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gulp
-zip -r election_theme.zip ./* -x './node_modules/*' './lib/*' './bower.json' './gulpfile.js' './release_script'
+zip -r election_theme.zip ./* -x './node_modules/*' './lib/*' './bower.json' './gulpfile.js' './release_script.sh'
